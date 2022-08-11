d55a66b3247c7920cf19c6490fdfd1c127b98fcf
Animesh Dhakal <dhakalanimesh385@gmail.com>
2022-08-11 20:26:52 +0545

    Fixed AppImage

--- scripts/build-app-image.sh
+++ scripts/build-app-image.sh
@@ -1,4 +1,4 @@
-mv images/TimeTracker.png appdir/TimeTracker.png
+mv images/icon.png appdir/TimeTracker.png
 mv desktop/TimeTracker.desktop appdir/TimeTracker.desktop
 
 wget -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
@@ -8,5 +8,3 @@ chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 wget -nv "https://github.com/probonopd/go-appimage/releases/download/continuous/appimagetool-715-x86_64.AppImage"
 chmod a+x appimagetool-715-x86_64.AppImage
 ./appimagetool-715-x86_64.AppImage appdir
-
-ls
