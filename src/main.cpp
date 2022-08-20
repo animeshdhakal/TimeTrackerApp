483c50b7f8575e5a162d8d27d26db5afaa2f1334
Animesh Dhakal <dhakalanimesh385@gmail.com>
2022-08-20 09:30:40 +0545

    Fixing Icon

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,15 @@ int main(int argc, char *argv[])
     Notification::init("TimeTracker");
     app.setApplicationName("TimeTracker");
 
+    #ifndef Q_OS_WIN
+        #include <winuser.h>
+        #include <windows.h>
+        
+        HICON hIcon = LoadIcon(hInstance, MAKEINTRESOURCE("images/icon.ico"));
+        SendMessage(hWnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
+        SendMessage(hWnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+    #endif
+
     UpdateChecker updateChecker;
     updateChecker.checkForUpdate();
 
