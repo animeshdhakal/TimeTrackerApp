f12927c5f3e4655a6c978fc28089853ccf6596bd
Animesh Dhakal <72594139+animeshdhakal@users.noreply.github.com>
2022-08-20 08:44:40 +0545

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,6 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    app.setWindowIcon(QIcon("images/icon.ico"));
-
     Notification::init("TimeTracker");
     app.setApplicationName("TimeTracker");
 
